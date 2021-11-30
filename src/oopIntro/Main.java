790026bcbd85b008fecbdd119715ab6fddd1cdf8
OnurKaganFindik <45922689+OnurKaganFindik@users.noreply.github.com>
2021-12-01 02:07:14 +0300

    Category

--- src/oopIntro/Main.java
+++ src/oopIntro/Main.java
@@ -3,13 +3,10 @@ package oopIntro;
 public class Main {
 
 	public static void main(String[] args) {
-		Product product1= new Product(); 
-		product1.id=1;
-		product1.name="Lenova V14";
-		product1.unitPrice=15000;
-		product1.detail="16 GB Ram";
+		Product product1= new Product(1,"Lenova V14",15000,"16 GB Ram"); //object creation of class Product with parameters 
+	
 		
-		Product product2= new Product(); 
+		Product product2= new Product(); //object creation of class Product with default parameters
 		product2.id=2;
 		product2.name="Lenova V15";
 		product2.unitPrice=12000;
@@ -24,7 +21,7 @@ public class Main {
 		
 		
 		
-		Product[] products= {product1,product2,product3};
+		Product[] products= {product1,product2,product3}; //creating an array of Product class and storing it in products variable 
 		
 		for (Product product : products) {
 			
@@ -34,6 +31,15 @@ public class Main {
 		
 		
 		System.out.println(products.length);
+
+		Category category1= new Category(); //object creation of class Category with default parameters
+		category1.id=1;
+		category1.name="Laptop";
+
+		Category category2= new Category();
+		category1.id=2;
+		category1.name="Ev/Bahçe";
+
 		
 	}
 
--- src/oopIntro/Product.java
+++ src/oopIntro/Product.java
@@ -1,6 +1,19 @@
 package oopIntro;
 
 public class Product {
+
+	public Product() { // default constructor
+		System.out.println("Product constructor");
+	};
+
+	public Product( int id ,String name, double unitPrice, String detail) { // parameterized constructor
+		System.out.println("Product constructor with parameters");
+		this.id = id;
+		this.name = name;
+		this.unitPrice = unitPrice;
+		this.detail = detail;
+	}
+
 	int id;
 	String name;
 	double unitPrice;
